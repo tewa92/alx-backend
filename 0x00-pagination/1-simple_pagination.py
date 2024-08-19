84d7f1f15b382cf8acc1aafc00be363299c67847
tewa92 <tewachewsaleigizer@gmail.com>
2024-08-19 07:40:37 -0700

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -32,7 +32,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert page == int and page_size == int
+        assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
 
         start, end = index_range(page, page_size)
