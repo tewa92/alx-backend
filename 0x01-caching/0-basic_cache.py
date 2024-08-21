54df6410a4fefd68ad3a6cb680ea6efbad09e7a4
tewa92 <tewachewsaleigizer@gmail.com>
2024-08-21 03:50:42 -0700

    caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
-''' BasicCache class that inherits from BaseCaching and is a caching system '''
+'''
+BasicCache class that inherits from BaseCaching and is a caching system
+'''
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """ BasicCache inherits from BaseCaching
     """
+
     def put(self, key, item):
-        """ Add an item in the cache
+        """Adds an item in the cache.
         """
-        if key and item:
-            self.cache_data[key] = item
-        if len(self.cache_data) > self.MAX_ITEMS:
-            keys = list(self.cache_data.keys())
-            first_item = keys[0]
-            del self.cache_data[first_item]
-            print("DISCARD: {}".format(first_item))
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key
         """
-        if key in self.cache_data:
-            return self.cache_data[key]
-        return None
+        return self.cache_data.get(key, None)
